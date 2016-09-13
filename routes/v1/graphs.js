f534d460ced91e66f485b06606f531e9d5ddcb06
Oscar Mendoza <oscar@smartwave.ph>
2016-09-13 11:05:36 +0800

    added a second response option for graphs

--- routes/v1/graphs.js
+++ routes/v1/graphs.js
@@ -3,9 +3,18 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-router.get("/", function(req, res, next) {
-	// console.log(__dirname);
+
+router.get("/one/", function(req, res, next) {
+	console.log((req.method + " called at /v1/graphs/one/").green);
 	fs.readFile(__dirname+'/mock_responses/graphs.json', function(err, file) {
+		res.json(JSON.parse(file));
+	});
+
+});
+
+router.get("/two/", function(req, res, next) {
+	console.log((req.method + " called at /v1/graphs/two/").green);
+	fs.readFile(__dirname+'/mock_responses/graphs_two.json', function(err, file) {
 		res.json(JSON.parse(file));
 	});
 
