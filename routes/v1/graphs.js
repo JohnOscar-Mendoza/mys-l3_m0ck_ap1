f46ad7143c5101037dfacf3dfa104d45d74a7fc4
Oscar Mendoza <oscar@smartwave.ph>
2016-09-13 17:28:55 +0800

    getting guage graphs

--- routes/v1/graphs.js
+++ routes/v1/graphs.js
@@ -4,20 +4,32 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-router.get("/one/", function(req, res, next) {
+router.get("/line/one/", function(req, res, next) {
 	console.log((req.method + " called at /v1/graphs/one/").green);
 	fs.readFile(__dirname+'/mock_responses/graphs.json', function(err, file) {
 		res.json(JSON.parse(file));
 	});
-
 });
 
-router.get("/two/", function(req, res, next) {
+router.get("/line/two/", function(req, res, next) {
 	console.log((req.method + " called at /v1/graphs/two/").green);
 	fs.readFile(__dirname+'/mock_responses/graphs_two.json', function(err, file) {
 		res.json(JSON.parse(file));
 	});
+});
 
+router.get("/guage/one/", function(req, res, next){
+	console.log((req.method + " called at /v1/graphs/guage/two/").green);
+	fs.readFile(__dirname+'/mock_responses/guage.json', function(err, file) {
+		res.json(JSON.parse(file));
+	});
+});
+
+router.get("/guage/two/", function(req, res, next){
+	console.log((req.method + " called at /v1/graphs/guage/two/").green);
+	fs.readFile(__dirname+'/mock_responses/guage_two.json', function(err, file) {
+		res.json(JSON.parse(file));
+	});
 });
 
 module.exports = router;
